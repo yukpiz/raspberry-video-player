e316c8f8842e04cbb376daa03a1ee99102838718
yukpiz <yukpiz@gmail.com>
2018-07-06 14:53:13 +0900

    Updated.

--- main.go
+++ main.go
@@ -2,46 +2,55 @@ package main
 
 import (
 	"fmt"
-	. "github.com/KeluDiao/gotube/api"
-	"log"
+	"github.com/jleight/omxplayer"
+	"github.com/otium/ytdl"
+	"os"
+	"path/filepath"
+	"strconv"
 )
 
 func main() {
 	fmt.Println("===> START MAIN")
-	//y := youtube.NewYoutube(true)
-	//y.DecodeURL("https://www.youtube.com/watch?v=6UoatE5DXJw")
-	//y.DecodeURL("https://www.youtube.com/watch?v=4jHNBzTe1iI")
-	//y.StartDownload("/home/yukpiz/labo/repos/private/raspberry-video-player/hoge.mp4")
+	url := "https://www.youtube.com/watch?v=6UoatE5DXJw"
+	fmt.Println("Download: " + url)
+	filename := download(url)
 
-	//vid, err := ytdl.GetVideoInfo("https://www.youtube.com/watch?v=6UoatE5DXJw")
-	//if err != nil {
-	//	fmt.Println(err)
-	//	os.Exit(1)
-	//}
-	//formatsFound := vid.Formats
-	//search := []string{"720p"}
-	//s := make([]interface{}, len(search))
-	//for i, v := range search {
-	//	s[i] = v
-	//}
-	//formats := formatsFound.Filter(ytdl.FormatResolutionKey, s)
+	dir, _ := filepath.Abs(filepath.Dir(filename))
+	play(filepath.Join(dir, filename))
+}
 
-	//file, _ := os.Create(vid.Title + ".mp4")
-	//defer file.Close()
-	//err = vid.Download(formats[0], file)
-	//if err != nil {
-	//	fmt.Println(err)
-	//}
-	//idList := [...]string{"6UoatE5DXJw"}
-	//rep := "Curry_highlights"
-	//for _, id := range idList {
-	//	vl, err := GetVideoListFromId(id)
-	//	if err != nil {
-	//		log.Fatal(err)
-	//	}
-	//	err = vl.Download(rep, "hoge.mp4", "720p", "video/mp4")
-	//	if err != nil {
-	//		log.Fatal(err)
-	//	}
-	//}
+func download(url string) string {
+	vid, _ := ytdl.GetVideoInfo(url)
+
+	var format ytdl.Format
+	var targetIndex int
+	for i, f := range vid.Formats {
+		if f.Extension == "mp4" {
+			format = f
+			targetIndex = i
+			break
+		}
+		fmt.Printf("[%d, %s, %s, %s, %s]\n", i, f.Extension, f.AudioEncoding, f.Resolution, strconv.Itoa(f.AudioBitrate))
+	}
+
+	fmt.Printf("Target ===> [%d, %s, %s, %s, %s]\n", targetIndex, format.Extension, format.AudioEncoding, format.Resolution, strconv.Itoa(format.AudioBitrate))
+	file, err := os.Create(vid.ID + "." + format.Extension)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	defer file.Close()
+	err = vid.Download(format, file)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	return file.Name()
+}
+
+func play(file string) {
+	fmt.Println("Playing: " + file)
+	player, err := omxplayer.New(file, "--no-osd")
+	fmt.Println(player)
+	fmt.Println(err)
 }
